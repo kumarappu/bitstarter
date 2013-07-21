c97d57efe1edfdfb1db18c2660b8cb09cd8b416b
Ubuntu <ubuntu@ip-10-236-143-85.ec2.internal>
2013-07-21 22:00:05 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World!, its my first app on heroku \n -appu');
 });
 
 var port = process.env.PORT || 5000;
